0318eaa739bec6ac2eca8106d2f7b2e4a12b91c0
dharshini-36 <dharshininatarajan584@gmail.com>
2026-04-04 11:43:01 +0530

    Update DharshiniAGI.py

--- DharshiniAGI.py
+++ DharshiniAGI.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
-st.set_page_config(page_title="Dharshini's AGI", page_icon="🧠")
+st.set_page_config(page_title="ChatBot", page_icon="🧠")
 
 st.title("Simple Chatbot")
 
